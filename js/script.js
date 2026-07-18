d607a7c332b7db536354033dff0d049a582399de
joaopaulo7319 <joaopaulo7319@gmail.com>
2026-07-17 23:04:36 -0300

    Projeto Planetas

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function trocarVenus(){
+    document.querySelector(".card-Mercurio").src="./img/Venus.png"
+}
+
+function trocarMarte(){
+    document.querySelector(".card-Terra").src="./img/Marte.PNG"
+}
+
+function trocarSaturno(){
+    document.querySelector(".card-Jupiter").src="./img/Saturno.webp"
+}
+
+function trocarNetuno(){
+    document.querySelector(".card-Urano").src="./img/Netuno.PNG"
+}
